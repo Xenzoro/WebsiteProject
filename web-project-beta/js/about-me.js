0864e99b0ba1356ca1c61949b6f6c7a1c79e9c16
origi <xxjacobmartinezxx@gmail.com>
2025-11-13 12:27:15 -0800

    Update About Me section and restructure project files: enhance content, adjust styles for better readability, and create new projects page

---
+++ web-project-beta/js/about-me.js
@@ -0,0 +1,5 @@
+// document.getElementById("about-me-container").scrollIntoView();
+// const scrollableDiv = document.getElementById("about-me-container");
+// scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
+// i think my other files are interfering with this
+window.scrollTo(0,document.body.scrollHeight);
